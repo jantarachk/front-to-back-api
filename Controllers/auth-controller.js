f90f349d68bd79260ca60b8d32eb97700c895481
jantarachk <jantarach.k@gmail.com>
2025-02-05 15:28:34 +0700

    update step 9

--- Controllers/auth-controller.js
+++ Controllers/auth-controller.js
@@ -1,4 +1,6 @@
 const createError = require("../Utilities/createError")
+const bcrypt = require("bcryptjs")
+const prisma = require("../Configs/prisma")
 
 exports.register = async (req,res,next) => {
     try {
@@ -19,11 +21,36 @@ exports.register = async (req,res,next) => {
         }
 
     //Step 3-check if exist
-    //Step 4-encrypt bcrypt
-    //Step 5-insert to db
-    //Step 6-response to front end
+        const checkEmail = await prisma.profile.findFirst({
+            where: {
+                email: email,
+                //ข้างหน้าชื่อตาราง ข้างหลัง value ที่เรารับมา
+            }
+        })
 
-        res.json({ message: "Hello register"})
+        console.log(checkEmail)
+        if (checkEmail) {
+            return createError(400, "Email is already exist")
+        }
+
+
+    //Step 4-encrypt bcrypt
+        const salt = bcrypt.genSaltSync(10)
+        // console.log(salt)
+        const hashedPassword = bcrypt.hashSync(password,salt)
+        // console.log(hashedPassword)
+
+    //Step 5-insert to db
+    const profile = await prisma.profile.create({
+        data:{
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            password: hashedPassword
+        }
+    })
+    //Step 6-response to front end
+        res.json({ message: "Register Success"})
     } catch (error) {
         console.log("step 2 catch error")
         next(error)
